1eb8026e65271fbc12472ebac22ebb5f777a4e1d
Diego Quintanilla <diego.quintanilla@proton.me>
2025-03-16 18:51:47 -0400

    add speed configuration for x and y axis

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -35,6 +35,20 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right until the key is released.
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # Move the ship to the left until the key is released.
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    # Stop moving the ship to the right.
+                    self.ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    # Stop moving the ship to the left.
+                    self.ship.moving_left = False
 
     def _update_screen(self) -> None:
         """
@@ -52,6 +66,8 @@ class AlienInvasion:
         """
         while True:
             self._check_events()
+
+            self.ship.update()
 
             # Redraw the screen during each pass through the
             # loop.
