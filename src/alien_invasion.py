4824cbc8c020b79b7067daf59422c55b5210910e
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-03-19 23:05:45 -0400

    add bullet functionality to main file

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -4,6 +4,7 @@ from pygame.event import Event
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -31,6 +32,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -66,6 +68,8 @@ class AlienInvasion:
         # this keyboard shortcut helps with exiting game in fullscreen mode.
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event: Event) -> None:
         """
@@ -81,7 +85,13 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             # Stop moving the ship to the left.
             self.ship.moving_left = False
-        pass
+
+    def _fire_bullet(self) -> None:
+        """
+        Create a new bullet and add it to the bullets group.
+        """
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self) -> None:
         """
@@ -89,6 +99,9 @@ class AlienInvasion:
         """
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
@@ -101,6 +114,7 @@ class AlienInvasion:
             self._check_events()
 
             self.ship.update()
+            self.bullets.update()
 
             # Redraw the screen during each pass through the
             # loop.
