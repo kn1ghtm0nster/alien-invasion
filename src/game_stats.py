bd0bbdedf3be078c6253f5e4fea7182f4e7659e9
Diego Quintanilla <diego.quintanilla@proton.me>
2025-04-05 16:02:25 -0400

    add game stats class

---
+++ src/game_stats.py
@@ -0,0 +1,22 @@
+"""
+Module contains the GameStats class.
+"""
+
+
+class GameStats:
+    """
+    Track statistics for Alien Invasion.
+    """
+
+    def __init__(self, ai_game):
+        """
+        Initialize statistics.
+        """
+        self.settings = ai_game.settings
+        self.reset_stats()
+
+    def reset_stats(self) -> None:
+        """
+        Initialize statistics that can change during the game.
+        """
+        self.ships_left = self.settings.ship_limit
